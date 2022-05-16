c3baf437ad1b7a83e881e018c7f6e4c2a02ca77f
wuqiangnan <2441277210@qq.com>
2022-05-16 22:56:13 +0800

    feat:study react-router

---
+++ src-router01/App.js
@@ -0,0 +1,28 @@
+import { Link, Route } from "react-router-dom";
+import Home from './views/Home/index'
+import Test from './views/Test/index'
+import './App.css';
+import './style/reset.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2 style={{ marginBottom: '10px', fontSize: '22px' }}>学习 react-router</h2>
+        <div className='app-container'>
+          <div>
+            <Link className='span-a' to="/home">Home</Link>
+            <Link className='span-a' to="/test">Test</Link>
+          </div>
+          <article style={{ marginLeft: '10px' }}>
+            我是自路由组件中的内容
+            <Route path="/home" component={Home} />
+            <Route path="/test" component={Test} />
+          </article>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src-router01/views/Home/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+
+function Home() {
+  return (
+    <div className='p-home-container'>
+      <div className='p-home-title'>
+        我是 Home 页面
+      </div>
+    </div>
+  )
+}
+
+export default Home
